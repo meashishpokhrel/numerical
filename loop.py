c02f23fa42220a62f77b5e3a973ffbdab7cf52db
unknown <meashishpokhrel@gmail.com>
2019-07-12 16:49:26 +0545

    first commit

---
+++ loop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 10:41:26 2019
+
+@author: Ashish Pokhrel
+"""
+a=5
+b=10
+
+    for (i=a;i<+b;i++):
+        
+        print('hello there')
+        
